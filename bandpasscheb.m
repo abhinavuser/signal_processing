df22a1610183ec2f2dd0d7fa235c5719ea4eb51b
Abhinav <83628416+abhinavuser@users.noreply.github.com>
2025-09-15 14:20:02 +0530

    Add files via upload

---
+++ bandpasscheb.m
@@ -0,0 +1,17 @@
+clear all;
+alphap=1;
+alphas=20;
+ws=[0.2*pi,0.4*pi];
+wp=[0.1*pi,0.5*pi];
+[n,wn]=buttord(wp/pi,ws/pi,alphap,alphas)
+[b,a]=cheby1(n,alphap,wn)
+w=0:0.01:pi;
+[h,ph]=freqz(b,a,w);
+m=20*log10(abs(h));
+an=angle(h);
+subplot(2,1,1), plot(ph/pi,m);grid;
+ylabel('Gain in dB');
+xlabel('Normalized frequency');
+subplot(2,1,2), plot(ph/pi,an);grid;
+ylabel('Phase in Radians');
+xlabel('Normalized frequency');
